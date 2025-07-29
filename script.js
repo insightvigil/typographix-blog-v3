d26d2a6268419b0849b5df3e0533506055c9a140
Adrián Vigil <insightvigil@gmail.com>
2025-07-29 11:11:01 -0600

    SASS converted

--- script.js
+++ script.js
@@ -43,3 +43,59 @@ const checkScroll = () => {
 
 //Event Listener for scroll event
     window.addEventListener('scroll', checkScroll);
+
+
+
+//Dark Mode ------------------------------
+
+const themeSwitcher = document.getElementById('theme-switcher');
+
+//Update Theme Icon and Text
+
+const updateThemeIcon = (isDarkMode) => {
+    
+    themeSwitcher.children[0].classList.replace(isDarkMode ? 'fa-sun': 'fa-moon', isDarkMode ? 'fa-moon' : 'fa-sun' )
+}
+
+//Determine if dark mode is prfefered
+function prefersDarkMode() {
+    return window.matchMedia && window.matchMedia('(prefers-color-scheme:dark)').matches;
+}
+
+// Set the theme based on the preference
+function setThemeBasedOnPrference() {
+ const isDarkMode = prefersDarkMode();
+ document.documentElement.setAttribute('data-theme',isDarkMode ? 'dark' : 'light');
+ updateThemeIcon(isDarkMode);
+}
+
+//Switch theme 
+switchTheme = () => {
+    const currentTheme = document.documentElement.getAttribute('data-theme');
+    const newTheme = currentTheme === 'dark' ? 'light': 'dark';
+    document.documentElement.setAttribute('data-theme',newTheme);
+    localStorage.setItem('theme',newTheme);
+    updateThemeIcon(newTheme === 'dark');
+}
+
+
+//Event Listener 
+themeSwitcher.addEventListener('click', switchTheme)
+
+//Check Local Storage For Theme 
+function initializeTheme() {
+    const savedTheme = localStorage.getItem('theme');
+    if(savedTheme) {
+        document.documentElement.setAttribute('data-theme', savedTheme);
+        updateThemeIcon(savedTheme === 'dark');
+    }
+    else {
+        setThemeBasedOnPrference();
+    }
+}
+
+//Listen for system theme changes
+window.matchMedia('(prefers-color-scheme:dark)').addEventListener('change', setThemeBasedOnPrference)
+
+//Initialize theme when the script loads
+initializeTheme();
