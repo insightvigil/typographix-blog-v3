3f8cd72ea52614fe0b4d31a429526a6179d127da
Adrián Vigil <insightvigil@gmail.com>
2025-07-28 22:25:14 -0600

    New Version

---
+++ README.md
@@ -0,0 +1,26 @@
+## Typographix blog v1 🚀
+
+This is blog page, developed with HTML and CSS. 
+
+### Applied technologies 🧑‍💻
+- HTML
+- CSS
+- JavaScript
+
+### Features V1 ⚙️
+- Interactive
+- Animations
+- FontAwesome 
+- Google Fonts
+- Flexbox
+
+### Features in this version ⚙️
+- Web Page Structured
+- Interactive Navbar
+
+You can check it out live [here](https://insightvigil.github.io/typographix-v2).
+
+### Contributors 🤝
+- [Adrián Vigil](https://github.com/insightvigil)
+
+#### Project developed during _The CSS Bootcamp_ by ZTM
---
+++ script.js
@@ -0,0 +1,45 @@
+// Skeleton Screen UI
+
+setTimeout(() => {
+    const skeletons = document.querySelectorAll('.skeleton')
+    console.log(skeletons);
+    const hiddens = document.querySelectorAll('.hidden')
+    console.log(hiddens)
+
+    for(const skeleton of skeletons) {skeleton.style.display ='none'}
+    for(const hidden of hiddens) { hidden.style.display ='block'}
+}, 3000)
+
+
+
+
+//Function to check if page is scrolled and adjust the logo size
+const checkScroll = () => {
+    const navbar = document.getElementById('navbar');
+    const logo = document.getElementById('logo');
+
+    let scrollPosition = window.scrollY;
+    console.log(scrollPosition);
+
+    //Add/remove 'scrolled class based on scroll position 
+    if(scrollPosition>20) {
+        navbar.classList.add('scrolled');
+    }
+    else {
+        navbar.classList.remove('scrolled');
+    }
+
+    //Calculate new font size based on scroll position
+    let newSize = 4.8 - (scrollPosition * 0.03); //Decrease by 0.03 for every pixel scrolled
+
+    // Clamping the font size between 1.5rem and 3rem
+    newSize = Math.max(2.4, newSize);
+    newSize = Math.min(4.8, newSize)
+
+    logo.style.fontSize = newSize + 'rem';
+
+    
+}
+
+//Event Listener for scroll event
+    window.addEventListener('scroll', checkScroll);
